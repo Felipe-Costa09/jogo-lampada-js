3f5070d0c91eefde3a50c54c84d16847fdfc54a4
Felipe Almeida <lipestores66@gmail.com>
2026-06-17 22:59:33 -0300

    mexendo no js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 console.log("Loading Server DataTransfer...");
 
 window.alert("Bem-Vindo ao Lamp Game. \nPara jogar pressione 'OK'");
+
+var acesa = false;
+var quebrada = false;
+
+function acenderLampada() {
+    document.getElementById("image").src = "img/lampada-acesa.jpg";
+}
+
+function quebrarLampada() {
+        if (quebrada==false){
+            quebrada=true;
+    
+    document.getElementById("image").src = "img/lampada-quebrada.jpg";
+        }
+}
+
+function apagarLampada(){
+    document.getElementById("image").src ="img/lampada-apagada.jpg";
+}
+
+function interruptor(){
+    if(quebrada==true) {
+        return;
+    }
+    if (acesa==false){
+        acenderLampada();
+    }
+    else{
+        apagarLampada();
+    }
+    acesa = !acesa;
+}
